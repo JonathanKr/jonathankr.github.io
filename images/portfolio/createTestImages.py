e6e110d2bbe4e36bcc5c05bf93866c9d0f6984ad
Jonathan Kron <58877865+JonathanKr@users.noreply.github.com>
2022-09-19 00:29:19 +0200

    new website test

--- images/portfolio/createTestImages.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import random
-
-for i in range(200):
-    img = Image.new("RGB", (100 + random.randint(0, 100), 100 + random.randint(0, 100)),
-                    color=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-    img.save(str(i) + ".jpg")
