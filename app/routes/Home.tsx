d6e1064360cf26176e0ba8387ed35be799505f5d
JonathanKr <post.david04@gmail.com>
2026-04-11 22:40:33 +0200

    back to basics, i gave up on webdesign

--- app/routes/Home.tsx
+++ app/routes/Home.tsx
@@ -1,5 +1,5 @@
-import Footer from "~/components/Footer";
-import HeroSection from "~/components/HeroSection";
+import { Link } from "react-router";
+
 import type { Route } from "./+types/Home";
 
 export function meta({}: Route.MetaArgs) {
@@ -8,193 +8,103 @@ export function meta({}: Route.MetaArgs) {
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center bg-teal-900">
-      <HeroSection overline="Hello I'm" heading="Jonathan Kron" />
-      <section className="z-10 mx-8 mb-10 max-w-3xl text-justify">
-        <h2 className="font-sans text-5xl leading-tight font-bold">
-          Publications
-        </h2>
-        <div className="mb-10 font-mono">
-          <div className="relative hover:cursor-pointer hover:underline">
-            <svg
-              className="absolute top-0 left-15 -z-1 overflow-visible"
-              // viewBox="0 0 200 30"
-              xmlns="http://www.w3.org/2000/svg"
-              opacity={0.5}
-            >
-              <path
-                fill="teal"
-                d="M49.5,-50.9C63.2,-47.4,73,-31,75.6,-13.7C78.2,3.6,73.6,21.7,61.4,28.4C49.1,35,29.2,30.2,13.2,35.7C-2.7,41.3,-14.7,57.3,-29.4,60.7C-44.2,64.1,-61.7,54.9,-69.1,40.6C-76.5,26.3,-73.8,6.9,-62.9,-3.5C-52,-13.9,-33,-15.3,-21.2,-19.2C-9.3,-23.1,-4.7,-29.5,6.6,-37.4C17.8,-45.2,35.7,-54.5,49.5,-50.9Z"
-              />
-            </svg>
-            <span className="">[1] Wavefront</span> <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
-            aliquid facere quod vel rem nostrum. Quis beatae praesentium
-            assumenda ex, autem quae fugit id incidunt reprehenderit totam
-            pariatur aliquid.
-          </div>
-          [
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            Github
-          </span>
-          ][
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            ResearchGate
-          </span>
-          ]
+    <div className="lg:max-w-1/2">
+      <h1 className="mb-5 font-sans text-6xl font-bold">Jonathan Kron</h1>
+      <p className="mb-5 text-justify font-serif">
+        I am a{" "}
+        <Link
+          to="https://www.th-koeln.de/en/academics/media-technology-masters-program_7573.php"
+          target="_blank"
+        >
+          Mediatechnology
+        </Link>{" "}
+        student at TH Köln (Cologne University of Applied Sciences). Throughout
+        my studies I specialized in machine learning and acoustical programming,
+        with many projects also involving web development. I am proficient in{" "}
+        <Link to="#" target="_blank">
+          Python
+        </Link>
+        ,{" "}
+        <Link to="#" target="_blank">
+          TypeScript
+        </Link>{" "}
+        and{" "}
+        <Link to="#" target="_blank">
+          Rust
+        </Link>
+        . Technologies and frameworks I have worked with include{" "}
+        <Link to="#" target="_blank">
+          React
+        </Link>
+        ,{" "}
+        <Link to="#" target="_blank">
+          bun, uv, angular, nestjs, pytorch, etc, etc
+        </Link>
+        .
+      </p>
+      <p className="mb-5 text-justify font-serif">
+        You can find me on{" "}
+        <Link to="https://github.com/JonathanKr" target="_blank">
+          Github
+        </Link>
+        ,{" "}
+        <Link to="https://www.youtube.com/@JonathanKron" target="_blank">
+          Youtube
+        </Link>{" "}
+        or{" "}
+        <Link
+          to="https://www.researchgate.net/profile/Jonathan-Kron"
+          target="_blank"
+        >
+          ResearchGate
+        </Link>
+        . Or feel free to send me an <Link to="mailto:#">E-Mail</Link> (:
+      </p>
+      <p className="mb-5 text-justify font-serif">
+        This website contains my
+        <ol>
+          <li>
+            <Link to="#projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="#publications">Publications</Link>
+          </li>
+          <li>
+            <Link to="#cv">CV</Link>
+          </li>
+        </ol>
+      </p>
+      <h2 className="my-5 font-sans text-4xl font-bold" id="projects">
+        Projects
+      </h2>
+      <div className="mb-10 text-justify font-serif">
+        <div className="hover:cursor-pointer hover:underline">
+          [1] Wavefront <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
+          aliquid facere quod vel rem nostrum. Quis beatae praesentium assumenda
+          ex, autem quae fugit id incidunt reprehenderit totam pariatur aliquid.
         </div>
-        <div className="mb-10 font-mono">
-          <div className="hover:cursor-pointer hover:underline">
-            [1] Wavefront <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
-            aliquid facere quod vel rem nostrum. Quis beatae praesentium
-            assumenda ex, autem quae fugit id incidunt reprehenderit totam
-            pariatur aliquid.
-          </div>
-          [
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            Github
-          </span>
-          ][
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            ResearchGate
-          </span>
-          ]
+        [<Link to="#">Github</Link>
+        ][
+        <Link to="#">ResearchGate</Link>]
+      </div>
+      <div className="mb-10 text-justify font-serif">
+        <div className="hover:cursor-pointer hover:underline">
+          [1] Wavefront <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
+          aliquid facere quod vel rem nostrum. Quis beatae praesentium assumenda
+          ex, autem quae fugit id incidunt reprehenderit totam pariatur aliquid.
         </div>
-        <div className="mb-10 font-mono">
-          <div className="hover:cursor-pointer hover:underline">
-            [1] Wavefront <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
-            aliquid facere quod vel rem nostrum. Quis beatae praesentium
-            assumenda ex, autem quae fugit id incidunt reprehenderit totam
-            pariatur aliquid.
-          </div>
-          [
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            Github
-          </span>
-          ][
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            ResearchGate
-          </span>
-          ]
-        </div>
-      </section>
-      <section className="mx-8 mb-10 max-w-3xl text-justify">
-        <h2 className="font-sans text-5xl leading-tight font-bold">Projects</h2>
-        <div className="mb-10 font-mono">
-          <div className="hover:cursor-pointer hover:underline">
-            [1] Wavefront <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
-            aliquid facere quod vel rem nostrum. Quis beatae praesentium
-            assumenda ex, autem quae fugit id incidunt reprehenderit totam
-            pariatur aliquid.
-          </div>
-          [
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            Github
-          </span>
-          ][
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            ResearchGate
-          </span>
-          ]
-        </div>
-        <div className="mb-10 font-mono">
-          <div className="hover:cursor-pointer hover:underline">
-            [1] Wavefront <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
-            aliquid facere quod vel rem nostrum. Quis beatae praesentium
-            assumenda ex, autem quae fugit id incidunt reprehenderit totam
-            pariatur aliquid.
-          </div>
-          [
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            Github
-          </span>
-          ][
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            ResearchGate
-          </span>
-          ]
-        </div>
-        <div className="mb-10 font-mono">
-          <div className="hover:cursor-pointer hover:underline">
-            [1] Wavefront <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
-            aliquid facere quod vel rem nostrum. Quis beatae praesentium
-            assumenda ex, autem quae fugit id incidunt reprehenderit totam
-            pariatur aliquid.
-          </div>
-          [
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            Github
-          </span>
-          ][
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            ResearchGate
-          </span>
-          ]
-        </div>
-      </section>
-      <section className="mx-8 mb-10 max-w-3xl text-justify">
-        <h2 className="font-sans text-5xl leading-tight font-bold">CV</h2>
-        <div className="mb-10 font-mono">
-          <div className="hover:cursor-pointer hover:underline">
-            [1] Wavefront <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
-            aliquid facere quod vel rem nostrum. Quis beatae praesentium
-            assumenda ex, autem quae fugit id incidunt reprehenderit totam
-            pariatur aliquid.
-          </div>
-          [
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            Github
-          </span>
-          ][
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            ResearchGate
-          </span>
-          ]
-        </div>
-        <div className="mb-10 font-mono">
-          <div className="hover:cursor-pointer hover:underline">
-            [1] Wavefront <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
-            aliquid facere quod vel rem nostrum. Quis beatae praesentium
-            assumenda ex, autem quae fugit id incidunt reprehenderit totam
-            pariatur aliquid.
-          </div>
-          [
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            Github
-          </span>
-          ][
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            ResearchGate
-          </span>
-          ]
-        </div>
-        <div className="mb-10 font-mono">
-          <div className="hover:cursor-pointer hover:underline">
-            [1] Wavefront <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, vero
-            aliquid facere quod vel rem nostrum. Quis beatae praesentium
-            assumenda ex, autem quae fugit id incidunt reprehenderit totam
-            pariatur aliquid.
-          </div>
-          [
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            Github
-          </span>
-          ][
-          <span className="text-indigo-300 hover:cursor-pointer hover:underline">
-            ResearchGate
-          </span>
-          ]
-        </div>
-      </section>
-      <Footer />
+        [<Link to="#">Github</Link>
+        ][
+        <Link to="#">ResearchGate</Link>]
+      </div>
+      <h2 className="my-5 font-sans text-4xl font-bold" id="publications">
+        Publications
+      </h2>
+      <h2 className="my-5 font-sans text-4xl font-bold" id="cv">
+        CV
+      </h2>
     </div>
   );
 };
