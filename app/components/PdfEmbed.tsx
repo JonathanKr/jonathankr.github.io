57b2da354769526e115ef82dd8f0a7f40986de3d
JonathanKr <post.david04@gmail.com>
2026-04-24 17:09:30 +0200

    test react pdf

--- app/components/PdfEmbed.tsx
+++ app/components/PdfEmbed.tsx
@@ -1,10 +1,65 @@
-interface PdfEmbedProps {
-  src: string;
-  height?: number;
-}
+import type { PDFDocumentProxy } from "pdfjs-dist/types/src/display/api";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 
-export default function PdfEmbed({ src, height = 600 }: PdfEmbedProps) {
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+
+const PdfEmbed = ({ src }: { src: string }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState<number>();
+  const [dpr, setDpr] = useState(window.devicePixelRatio);
+  const [numPages, setNumPages] = useState<number>();
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+    const observer = new ResizeObserver(([entry]) => {
+      setWidth(entry.contentRect.width);
+    });
+    observer.observe(containerRef.current);
+    return () => observer.disconnect();
+  }, []);
+
+  useEffect(() => {
+    const updateDpr = () => {
+      setDpr(window.devicePixelRatio);
+      const mql = window.matchMedia(
+        `(resolution: ${window.devicePixelRatio}dppx)`
+      );
+      mql.addEventListener("change", updateDpr, { once: true });
+    };
+    const mql = window.matchMedia(
+      `(resolution: ${window.devicePixelRatio}dppx)`
+    );
+    mql.addEventListener("change", updateDpr, { once: true });
+    return () => mql.removeEventListener("change", updateDpr);
+  }, []);
+
+  function onDocumentLoadSuccess({
+    numPages: nextNumPages,
+  }: PDFDocumentProxy): void {
+    setNumPages(nextNumPages);
+  }
+
   return (
-    <embed src={src} type="application/pdf" width="100%" height={height} />
+    <div ref={containerRef}>
+      <Document file={src} onLoadSuccess={onDocumentLoadSuccess}>
+        {Array.from(new Array(numPages), (_el, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            width={width}
+            devicePixelRatio={dpr}
+          />
+        ))}
+      </Document>
+    </div>
   );
-}
+};
+
+export default PdfEmbed;
