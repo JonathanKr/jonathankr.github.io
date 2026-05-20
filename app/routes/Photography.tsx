2762d6a856cdd5feee35b395c4ad3906e234f51e
JonathanKr <post.david04@gmail.com>
2026-05-20 20:15:04 +0200

    test photos

--- app/routes/Photography.tsx
+++ app/routes/Photography.tsx
@@ -1,9 +1,16 @@
 import Page from "~/components/Page";
+import imageIndex from "~/images/index.json";
 
 const Photography = () => {
+  const images = imageIndex.images;
+
   return (
     <Page>
       <h1>Photography</h1>
+      Nothing you see here is AI-generated.
+      {images.map((e) => (
+        <img key={e.path} src={e.path} alt={e.title} className="mt-3" />
+      ))}
     </Page>
   );
 };
