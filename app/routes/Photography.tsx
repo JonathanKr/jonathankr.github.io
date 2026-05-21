6924b2e5af3640aacb5429a09a0e374757f7c6b6
JonathanKr <post.david04@gmail.com>
2026-05-21 12:42:46 +0200

    lazy load

--- app/routes/Photography.tsx
+++ app/routes/Photography.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
+
 import Page from "~/components/Page";
 import imageIndex from "~/images/index.json";
 
 const Photography = () => {
   const images = imageIndex.images;
 
+  const [loadedCount, setLoadedCount] = useState(1);
+
+  const handleLoad = (index: number) => {
+    if (index + 1 === loadedCount) {
+      setLoadedCount((c) => c + 1);
+    }
+  };
+
   return (
     <Page>
       <h1>Photography</h1>
       Nothing you see here is AI-generated.
-      {images.map((e) => (
-        <img key={e.path} src={e.path} alt={e.title} className="mt-3" />
+      {images.map((e, index) => (
+        <img
+          key={e.path}
+          src={index < loadedCount ? e.path : undefined}
+          alt={e.title}
+          className="mt-2 mb-4"
+          onLoad={() => handleLoad(index)}
+        />
       ))}
     </Page>
   );
